ff5b46a3cdac3378be6f557c936c74c1693b23d2
aymen-trabelsi <a.trabelsi2027@gmail.com>
2021-12-20 14:37:14 +0100

    First Commit
    Create a new project

---
+++ src/test/java/gestion/authentification/biblio/BiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package gestion.authentification.biblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
